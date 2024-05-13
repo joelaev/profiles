d5667bce3269aff5fbda47b0732bcb2e4b4b8881
Joelaev <escobarvj117@gmail.com>
2024-05-13 15:46:38 -0600

    ⭐Feat: Githun User card final ultima parte de la actividad

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
-<script setup></script>
+<script setup>
+import GithubCard from './components/GithubCard.vue'
+</script>
 
 <template>
-  <div class="card card-side bg-base-100 shadow-xl">
-    <figure>
-      <img
-        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-        alt="Movie"
-      />
-    </figure>
-    <div class="card-body">
-      <h2 class="card-title">New movie is released!</h2>
-      <p>Click the button to watch on Jetflix app.</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Watch</button>
-      </div>
-    </div>
+  <div class="">
+    <GithubCard username="joelaev" />
+    <GithubCard username="rivalcoba" />
+    <GithubCard username="BrunoPCH" />
+    <GithubCard username="Yampyyii" />
   </div>
 </template>
 
